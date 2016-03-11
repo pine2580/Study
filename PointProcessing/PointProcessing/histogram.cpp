5151e4234a7ade5b0b441b674a1ca612ea88d011
SongRan <pine0814@gmail.com>
2016-03-11 14:48:54 +0900

    add Equalize

--- PointProcessing/PointProcessing/histogram.cpp
+++ PointProcessing/PointProcessing/histogram.cpp
@@ -48,11 +48,47 @@ void End_IN(Mat in, int max_temp, int min_temp, Mat out)
 				out.at<uchar>(i,j)= ((in.at<uchar>(i, j) - min_temp) * 255 / (max_temp - min_temp));
 		}
 }
+void Equalize(Mat in, int max_temp,Mat out)
+{
+	//평활화
+	int hist[256] = { 0 };
+	int sum[256] = { 0 };
+	int n[256] = { 0 };
+	//1.히스토그램 생성
+	for (int k = 0; k < 256;k++)
+		for (int i = 0; i < in.rows; i++)
+			for (int j = 0; j < in.cols; j++)
+				{
+					if (in.at<uchar>(i, j) == k)
+						hist[k] += 1;
+				}
+	//2.누적합 생성
+	for (int i = 0; i < 256; i++)
+	{
+		if (i == 0)
+			sum[i] = hist[i];
+		else
+			sum[i] += (hist[i]+sum[i-1]);
+	}
+	//3. 정규화
+	for (int i = 0; i < 256; i++)
+	{
+		n[i] = (sum[i] * max_temp )/ (256*256) ;
+	}
+	for (int k = 0; k < 256; k++)
+		for (int i = 0; i < in.rows; i++)
+			for (int j = 0; j < in.cols; j++)
+			{
+				if (in.at<uchar>(i, j) == k)
+					out.at<uchar>(i, j) = n[k];
+			}
+}
 void main()
 {
 	Mat A=imread("lenna.bmp", CV_LOAD_IMAGE_GRAYSCALE);
 	Mat B(A.rows, A.cols, CV_8UC1);
 	Mat C(A.rows, A.cols, CV_8UC1);
+	Mat D(A.rows, A.cols, CV_8UC1);
 	int high=0, low=0;
 
 	high=PixelMax(A,high);
@@ -62,4 +98,6 @@ void main()
 	imwrite("HistoStretch.bmp", B);
 	End_IN(A, high, low, C);
 	imwrite("End_In.bmp", C);
+	Equalize(A, high, D);
+	imwrite("Equalize.bmp", D);
 }
